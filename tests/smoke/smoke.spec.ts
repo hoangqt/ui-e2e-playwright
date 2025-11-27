3ad0263dde25e431c6bf69c5507200e0b69050ea
Hoang Tran <hoangqt@gmail.com>
2025-11-27 11:35:43 -0500

    Add WCAG

--- tests/smoke/smoke.spec.ts
+++ tests/smoke/smoke.spec.ts
@@ -2,30 +2,40 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/LoginPage";
 import { ProductsPage } from "../../pages/ProductsPage";
 import { testUsers } from "../../data/users";
+import { AxeBuilder } from "@axe-core/playwright";
 
 test.describe("Smoke testing", () => {
-  test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.login(
-      testUsers.standardUser.username,
-      testUsers.standardUser.password,
-    );
-    expect(await loginPage.isSuccessMessageVisible()).toBe(true);
-  });
+    test.beforeEach(async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.login(
+            testUsers.standardUser.username,
+            testUsers.standardUser.password,
+        );
+        expect(await loginPage.isSuccessMessageVisible()).toBe(true);
+    });
 
-  test("should log in successfully", async ({ page }) => {
-    const productsPage = new ProductsPage(page);
-    expect(await productsPage.isProductsPageVisible()).toBe(true);
-  });
+    test("should log in successfully", async ({ page }) => {
+        const productsPage = new ProductsPage(page);
+        expect(await productsPage.isProductsPageVisible()).toBe(true);
+    });
 
-  test("should add product to cart and checkout", async ({ page }) => {
-    const productsPage = new ProductsPage(page);
-    await productsPage.goto();
-    expect(await productsPage.isProductsPageVisible()).toBe(true);
+    test("should add product to cart and checkout", async ({ page }) => {
+        const productsPage = new ProductsPage(page);
+        await productsPage.goto();
+        expect(await productsPage.isProductsPageVisible()).toBe(true);
 
-    await productsPage.addProductToCart("Sauce Labs Backpack");
-    await productsPage.cart();
-    await productsPage.checkout();
-  });
+        await productsPage.addProductToCart("Sauce Labs Backpack");
+        await productsPage.cart();
+        await productsPage.checkout();
+    });
+
+    test("should have accessibility violations", async ({ page }) => {
+        // Check against wcag2a and wcag21aa rules
+        const accessibilityResults = await new AxeBuilder({ page })
+            .withTags(["wcag2a", "wcag21aa"])
+            .analyze();
+        // This page is not WCAG compliant so expect violations to be greater than 0
+        expect(accessibilityResults.violations.length).toBeGreaterThan(0);
+    });
 });
