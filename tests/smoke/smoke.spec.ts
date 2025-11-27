0d21b71fe7d0860847119a4a95f7101d2556707b
Hoang Tran <hoangqt@gmail.com>
2025-11-27 09:41:37 -0500

    Fix formatting

--- tests/smoke/smoke.spec.ts
+++ tests/smoke/smoke.spec.ts
@@ -4,28 +4,28 @@ import { ProductsPage } from "../../pages/ProductsPage";
 import { testUsers } from "../../data/users";
 
 test.describe("Smoke testing", () => {
-    test.beforeEach(async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.goto();
-        await loginPage.login(
-            testUsers.standardUser.username,
-            testUsers.standardUser.password,
-        );
-        expect(await loginPage.isSuccessMessageVisible()).toBe(true);
-    });
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(
+      testUsers.standardUser.username,
+      testUsers.standardUser.password,
+    );
+    expect(await loginPage.isSuccessMessageVisible()).toBe(true);
+  });
 
-    test("should log in successfully", async ({ page }) => {
-        const productsPage = new ProductsPage(page);
-        expect(await productsPage.isProductsPageVisible()).toBe(true);
-    });
+  test("should log in successfully", async ({ page }) => {
+    const productsPage = new ProductsPage(page);
+    expect(await productsPage.isProductsPageVisible()).toBe(true);
+  });
 
-    test("should add product to cart and checkout", async ({ page }) => {
-        const productsPage = new ProductsPage(page);
-        await productsPage.goto();
-        expect(await productsPage.isProductsPageVisible()).toBe(true);
+  test("should add product to cart and checkout", async ({ page }) => {
+    const productsPage = new ProductsPage(page);
+    await productsPage.goto();
+    expect(await productsPage.isProductsPageVisible()).toBe(true);
 
-        await productsPage.addProductToCart("Sauce Labs Backpack");
-        await productsPage.cart();
-        await productsPage.checkout();
-    });
+    await productsPage.addProductToCart("Sauce Labs Backpack");
+    await productsPage.cart();
+    await productsPage.checkout();
+  });
 });
