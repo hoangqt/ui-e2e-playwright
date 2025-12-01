f27bcd688974cd9171828faa33bd170f3d3ed950
Hoang Tran <hoangqt@gmail.com>
2025-11-30 20:29:12 -0500

    Fix formatting

--- pages/Products.ts
+++ pages/Products.ts
@@ -1,39 +1,39 @@
 export class Products {
-    constructor(private page: any) { }
+  constructor(private page: any) {}
 
-    async goto() {
-        await this.page.goto("https://www.saucedemo.com/inventory.html");
-    }
+  async goto() {
+    await this.page.goto("https://www.saucedemo.com/inventory.html");
+  }
 
-    async isProductsPageVisible() {
-        await this.page.waitForSelector('[data-test="title"]', {
-            state: "visible",
-            timeout: 5000,
-        });
-        return true;
-    }
+  async isProductsPageVisible() {
+    await this.page.waitForSelector('[data-test="title"]', {
+      state: "visible",
+      timeout: 5000,
+    });
+    return true;
+  }
 
-    async addProductToCart(productName: string) {
-        await this.page.getByText(productName).click();
-    }
+  async addProductToCart(productName: string) {
+    await this.page.getByText(productName).click();
+  }
 
-    async removeProductFromCart(productName: string) {
-        await this.page.getByText(productName).click();
-    }
+  async removeProductFromCart(productName: string) {
+    await this.page.getByText(productName).click();
+  }
 
-    async cart() {
-        await this.page.click('[data-test="shopping-cart-link"]');
-    }
+  async cart() {
+    await this.page.click('[data-test="shopping-cart-link"]');
+  }
 
-    async cartContinueShopping() {
-        await this.page.click('[data-test="continue-shopping"]');
-    }
+  async cartContinueShopping() {
+    await this.page.click('[data-test="continue-shopping"]');
+  }
 
-    async cartCheckout() {
-        await this.page.click('[data-test="checkout"]');
-    }
+  async cartCheckout() {
+    await this.page.click('[data-test="checkout"]');
+  }
 
-    async checkout() {
-        await this.page.getByText("Checkout", { exact: true }).click();
-    }
+  async checkout() {
+    await this.page.getByText("Checkout", { exact: true }).click();
+  }
 }
