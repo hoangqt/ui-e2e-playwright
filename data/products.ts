8c5f3ae6d613aa145b3959f227ad6f1536c763ee
Hoang Tran <hoangqt@gmail.com>
2025-12-02 20:12:51 -0500

    Add more products

--- data/products.ts
+++ data/products.ts
@@ -1,5 +1,8 @@
 export const testProducts = {
     backpack: "Sauce Labs Backpack",
     bikeLight: "Sauce Labs Bike Light",
-    tShirt: "Sauce Labs T-Shirt",
+    tShirt: "Sauce Labs Bolt T-Shirt",
+    fleece: "Sauce Labs Fleece Jacket",
+    onesie: "Sauce Labs Onesie",
+    redTShirt: "Test.allTheThings() T-Shirt (Red)"
 };
